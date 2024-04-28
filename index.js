2ee2a7071a845fbe40eb8c0f5bd51cdbd87695ac
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-04-28 08:43:56 +0600

    Api create for delete Craft of a user

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djweinm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -16,58 +15,68 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     const database = client.db("artCraftDB");
     const craftCollections = database.collection("crafts");
     const subcategoryCollections = database.collection("subcategory");
 
-    app.get('/crafts',async(req,res)=>{
-        const allCrafts = craftCollections.find();
-        const result = await allCrafts.toArray();
-        res.send(result)
-    })
+    // get all the crafts
+    app.get("/crafts", async (req, res) => {
+      const allCrafts = craftCollections.find();
+      const result = await allCrafts.toArray();
+      res.send(result);
+    });
 
-    app.get('/subcategories',async(req,res)=>{
-        const allSubcategories = subcategoryCollections.find();
-        const result = await allSubcategories.toArray();
-        res.send(result)
-    })
+    // get all the subcategories
+    app.get("/subcategories", async (req, res) => {
+      const allSubcategories = subcategoryCollections.find();
+      const result = await allSubcategories.toArray();
+      res.send(result);
+    });
 
+    // get all the craft of a user added
     app.get("/crafts/:user_email", async (req, res) => {
-        const email = req.params.user_email;
-        const query = { user_email: email };
-        const usersCrafts =  craftCollections.find(query);
-        const result = await usersCrafts.toArray();
-        res.send(result);
-      });
+      const email = req.params.user_email;
+      const query = { user_email: email };
+      const usersCrafts = craftCollections.find(query);
+      const result = await usersCrafts.toArray();
+      res.send(result);
+    });
 
-
+    // get the details of a particular craft
     app.get("/crafts/details/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await craftCollections.findOne(query);
-        res.send(result);
-      });
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollections.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/addCrafts',async (req,res)=>{
-        const newCraft = req.body
-        console.log(newCraft);
-        const result = await craftCollections.insertOne(newCraft)
-        res.send(result)
+    // Add craft
+    app.post("/addCrafts", async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollections.insertOne(newCraft);
+      res.send(result);
+    });
+
+    // delete a craft 
+    app.delete('/crafts/:id', async (req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollections.deleteOne(query)
+      res.send(result)
     })
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Testing the Server");
